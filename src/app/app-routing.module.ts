2a935560e50aa118eeb8bc49ccdc613ed7a45f28
casaxmore <casaxmore@gmail.com>
2023-02-27 20:46:56 +0100

    useHash: false para controlarlo desde node

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,24 +5,29 @@ import { ValidarTokenGuard } from './guards/validar-token.guard';
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('../app/protected/protected.module').then(m => m.ProtectedModule),
+    loadChildren: () =>
+      import('../app/protected/protected.module').then(
+        (m) => m.ProtectedModule
+      ),
     canActivate: [ValidarTokenGuard],
-    canLoad: [ValidarTokenGuard]
+    canLoad: [ValidarTokenGuard],
   },
   {
     path: '**',
-    redirectTo: 'auth'
-  }
+    redirectTo: 'auth',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: true // Forma de trabajar con Hash para evitar problemas de rotas en los navegadores web en producción
-  })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: false, // Forma de trabajar con Hash para evitar problemas de rutas en los navegadores web en producción, pero si queremos controlarlo desde node lo ponemos a false
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
