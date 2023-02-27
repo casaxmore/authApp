c01d077643a487ce285a8ecbae679ea9b7ffc864
casaxmore <casaxmore@gmail.com>
2023-02-27 15:47:58 +0100

    Maquetación login|registro y creación servicio

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    name: ['casaXmore', [Validators.required]],
+    email: ['casaxmore@gmail.com', [Validators.required, Validators.email]],
+    password: ['666666', [Validators.required, Validators.minLength(6)]],
+  })
+
+  constructor(private fb: FormBuilder, private router: Router){}
+
+  register(){
+    console.log(this.miFormulario.value);
+
+    this.router.navigateByUrl('/dashboard');
+  }
 
 }
